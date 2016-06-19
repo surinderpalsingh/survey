9f8c1eb2862b50889fdadef684493fa834c6db8c
Surinderpal Singh <mannecheema@gmail.com>
2016-06-19 22:16:16 +1000

    Added data folder for livereload

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,13 @@ gulp.task('develop', function () {
   livereload.listen();
   nodemon({
     script: 'app.js',
-    ext: 'js coffee jade',
+    ext: 'js jade json',
     stdout: false
   }).on('readable', function () {
     this.stdout.on('data', function (chunk) {
       if(/^Express server listening on port/.test(chunk)){
         livereload.changed(__dirname);
+        livereload.changed(__dirname + '../data');
       }
     });
     this.stdout.pipe(process.stdout);
